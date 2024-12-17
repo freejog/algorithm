732e1da9dfe93e1b4c5147b00eeb189bd1c090d5
freejog <167161823+freejog@users.noreply.github.com>
2024-12-17 23:05:10 +0900

    Update select_sort.rb
    選択ソート
    昇順、降順

--- ruby/select_sort.rb
+++ ruby/select_sort.rb
@@ -1,20 +1,41 @@
-def select_sort(array)
-  (0...array.length).each do |i|
-    min = array[i]
-    min_position = i
-    ((i+1)...array.length).each do |j|
-      if min > array[j]
-        min = array[j]
-        min_position = j
+# #昇順ソート
+# def select_sort(ary)
+#   length = ary.length
+#   (0...length).each do |i|
+#     min = ary[i]
+#     min_position = i
+#     ((i+1)...length).each do |j|
+#       if ary[j] < min
+#         min = ary[j]
+#         min_position = j
+#       end
+#     end
+#     tmp = ary[i]
+#     ary[i] = min
+#     ary[min_position] = tmp
+#   end
+#   return ary
+# end
+
+#降順ソート
+def select_sort(ary)
+  length = ary.length
+  (0...length).each do |i|
+    max = ary[i]
+    max_position = i
+    ((i+1)...length).each do |j|
+      if ary[j] > max
+        max = ary[j]
+        max_position = j
       end
     end
-    tmp = array[i]
-    array[i] = min
-    array[min_position] = tmp
+    tmp = ary[i]
+    ary[i] = max
+    ary[max_position] = tmp
   end
-  return array
+  return ary
 end
 
-array = [245, 45, 32, 367, 67, 14, 89]
-p array
-p select_sort(array)
+ary = [245, 45, 32, 367, 67, 14, 89]
+p ary
+p select_sort(ary)
